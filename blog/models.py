33a3a713ebd3f73024f6c4fb7abc5bb4bf5d4cee
JoaoHigino <jhigino10@hotmail.com>
2022-12-08 20:53:54 +0000

    Create a database model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Save for later"), (1, "Published"))
+
+
+class Recipe(models.Model):
+    """Model for Recipe"""
+    title = models.CharField(max_length=50, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipes")
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    preparation_time = models.CharField(max_length=10, default=0)
+    cook_time = models.CharField(max_length=10, default=0)
+    description = models.TextField()
+    ingredients = models.TextField(validators=[textfield_not_empty])
+    method = models.TextField(validators=[textfield_not_empty])
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=1)
+    bookmarks = models.ManyToManyField(
+        User, related_name='bookmark', default=None, blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"

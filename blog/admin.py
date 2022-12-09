f691ba7472b3bb389797aa4b95b05781317651a5
JoaoHigino <jhigino10@hotmail.com>
2022-12-09 09:15:24 +0000

    Migrate django summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
+"""imports for admin page"""
 from django.contrib import admin
+from .models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    """Allows admin to manage recipes via the admin panel"""
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'description')
+    summernote_fields = ('description', 'method')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Allows admin to manage comments on recipes via the admin panel"""
+    list_display = ('name', 'body', 'recipe', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'body')
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Save for later"), (1, "Published"))
@@ -8,23 +9,21 @@ STATUS = ((0, "Save for later"), (1, "Published"))
 """Model for Recipe"""
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+class Recipe(models.Model):
+    title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="recipes"
-    )
-    featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
+        User, on_delete=models.CASCADE, related_name="recipes")
+    created_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     preparation_time = models.CharField(max_length=50, default=0)
     cook_time = models.CharField(max_length=50, default=0)
     description = models.TextField()
-    status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+    ingredients = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=1)
+    bookmarks = models.ManyToManyField(
+        User, related_name='bookmark', default=None, blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -37,16 +36,17 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+    """Model for Comment"""
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_on"]
+        """ To display the comments by created_on in ascending order """
+        ordering = ['created_on']
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"

44e83a53bca98bba39d0904e36d9d1e1aede4ae9
JoaoHigino <jhigino10@hotmail.com>
2022-12-11 10:17:28 +0000

    Add recipe details view and update view.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,31 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = "index.html"
     paginate_by = 6
+
+
+class RecipeDetail(View):
+    def get(self, request, slug):
+        queryset = Recipe.objects.all()
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('created_on')
+        bookmarked = False
+        if recipe.bookmarks.filter(id=self.request.user.id).exists():
+            bookmarked = True
+
+        return render(
+            request,
+            "details.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked
+            },
+        )
+
+    def post(self, request, slug):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('created_on')
+        bookmarked = False
+        if recipe.bookmarks.filter(id=self.request.user.id).exists():
+            bookmarked = True

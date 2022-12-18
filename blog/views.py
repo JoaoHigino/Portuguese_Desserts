40da28064ac3172315d61e5488badb8458d80748
JoaoHigino <jhigino10@hotmail.com>
2022-12-18 10:01:01 +0000

    Add post likes

--- blog/views.py
+++ blog/views.py
@@ -36,3 +36,15 @@ class RecipeDetail(View):
         bookmarked = False
         if recipe.bookmarks.filter(id=self.request.user.id).exists():
             bookmarked = True
+
+
+class PostLike(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))

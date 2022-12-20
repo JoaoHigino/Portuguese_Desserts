0dcc181ac85959cb48bbf5993a8821e6bbe8e0f3
JoaoHigino <jhigino10@hotmail.com>
2022-12-20 13:54:10 +0000

    Add recipes search

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
 from . import views
+from django.urls import path, reverse
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home'),
-    path('about/', views.about, name='about'),
-    path('books/', views.create_recipe, name='books'),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name='details'),
+    path("", views.RecipeList.as_view(), name="home"),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='post_like'),
-
+    path(
+        'about',
+        views.generic.TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    path('search_articles', views.search_articles, name='search_articles'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -76,3 +76,19 @@ def create_recipe(request):
         recipe_form = RecipeForm()
     return render(request, "create_recipe.html", context)
 
+
+def search_articles(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        if not searched:
+            return redirect("/")
+        post_list = Post.objects.filter(
+            Q(content__icontains=searched) |
+            Q(title__icontains=searched) |
+            Q(author__username__icontains=searched)).filter(status=1)
+
+        return render(
+            request, 'index.html',
+            {'searched': searched, 'post_list': post_list})
+    else:
+        return render(request, 'index.html', {})

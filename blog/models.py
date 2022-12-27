c6591fd0148af54ad21a617c3cb7649eb562092a
JoaoHigino <jhigino10@hotmail.com>
2022-12-27 10:46:42 +0000

    Remove contact form and delete pagination.html

--- blog/models.py
+++ blog/models.py
@@ -53,13 +53,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.user}"
-
-
-class Contact(models.Model):
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name

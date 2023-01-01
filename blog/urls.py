7812879b5a47bd85a6ac31716a4b4674dee1c71c
JoaoHigino <jhigino10@hotmail.com>
2023-01-01 09:55:28 +0000

    Add search articles

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
         'about',
         views.generic.TemplateView.as_view(template_name='about.html'),
         name='about'),
+    path('search_articles', views.search_articles, name='search_articles'),
     path('new', views.CreatePostView.as_view(), name='new'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path(
--- blog/views.py
+++ blog/views.py
@@ -84,6 +84,23 @@ class PostDetail(View):
         )
 
 
+def search_articles(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        if not searched:
+            return redirect("/")
+        post_list = Post.objects.filter(
+            Q(content__icontains=searched) |
+            Q(title__icontains=searched) |
+            Q(author__username__icontains=searched)).filter(status=1)
+
+        return render(
+            request, 'index.html',
+            {'searched': searched, 'post_list': post_list})
+    else:
+        return render(request, 'index.html', {})
+
+
 class PostLike(View):
 
     def post(self, request, slug, *args, **kwargs):

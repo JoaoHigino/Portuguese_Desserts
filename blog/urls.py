e52292b539f4d994bc005e83f055cc4822862507
JoaoHigino <jhigino10@hotmail.com>
2023-01-03 10:42:27 +0000

    Delete search bar

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
         'about',
         views.generic.TemplateView.as_view(template_name='about.html'),
         name='about'),
-    path('search_articles', views.search_articles, name='search_articles'),
     path('new', views.CreatePostView.as_view(), name='new'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path(
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .models import Post
 from .forms import CommentForm
 from django.forms import Form
 from django.http import HttpResponseRedirect
-from django.db.models import Q
 from django.contrib import messages
 from django.views.generic import CreateView, ListView, DeleteView
 
@@ -82,23 +81,6 @@ class PostDetail(View):
                 "liked": liked
             },
         )
-
-
-def search_articles(request):
-    if request.method == 'POST':
-        searched = request.POST['searched']
-        if not searched:
-            return redirect("/")
-        post_list = Post.objects.filter(
-            Q(content__icontains=searched) |
-            Q(title__icontains=searched) |
-            Q(author__username__icontains=searched)).filter(status=1)
-
-        return render(
-            request, 'index.html',
-            {'searched': searched, 'post_list': post_list})
-    else:
-        return render(request, 'index.html', {})
 
 
 class PostLike(View):
